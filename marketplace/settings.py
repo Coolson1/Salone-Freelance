f41ba09f9e4741a29ab2660ce0362e13948f029b
Coolson1 <abdul1ck2@gmail.com>
2026-02-27 19:31:34 +0000

    Changed the allowed host

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -35,9 +35,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'replace-this-with-a-secure-key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://salone-freelance.onrender.com', 'salone-freelance.onrender.com']
 
 # Application definition
 
